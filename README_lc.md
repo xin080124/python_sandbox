a5d4cec4023c03218dac6d985cf52d225144696f
NancyZhang <Nancy.Zhang@fisherpaykel.com>
2022-08-10 15:02:18 +1200

    should add entry file

---
+++ README_lc.md
@@ -0,0 +1,8 @@
+
+To load the live data:
+
+```bash
+export PYTHONPATH=$(pwd)
+
+python3 my_python_entry.py --argu_demo nancy-src
+```
---
+++ my_python_entry.py
@@ -0,0 +1,11 @@
+
+
+def main():
+
+    parser = argparse.ArgumentParser(description='Loading demo data')
+    parser.add_argument('--argu_demo', help='Please provede argu_demo')
+    args = parser.parse_args()
+
+    
+if __name__ == '__main__':
+    main()
