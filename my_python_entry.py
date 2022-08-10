12780d6bbd82970964ab53039c7fe3414c536ab1
NancyZhang <Nancy.Zhang@fisherpaykel.com>
2022-08-10 16:31:21 +1200

    should enable args

--- my_python_entry.py
+++ my_python_entry.py
@@ -1,4 +1,5 @@
 
+import argparse
 
 def main():
 
@@ -6,6 +7,9 @@ def main():
     parser.add_argument('--argu_demo', help='Please provede argu_demo')
     args = parser.parse_args()
 
+    argu_demo = args.argu_demo
+
+    print("argu_demo", argu_demo)
     
 if __name__ == '__main__':
     main()
